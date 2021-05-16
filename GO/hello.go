66bec92c52ac34e3072bf9e54a1c8ccf4b1bfaa9
urbanjoe22 <joegxu22@gmail.com>
2021-05-16 19:49:05 -0400

    revisiting Go

--- GO/hello.go
+++ GO/hello.go
@@ -4,10 +4,12 @@ import (
 	"fmt"
 	"math/rand"
 	"time"
+	"rsc.io/quote"
 )
 
 func main() {
 	rand.Seed(time.Now().UTC().UnixNano())
-	fmt.Printf("Hello Go World\n")
+	fmt.Printf("Hello Go World!\n")
 	fmt.Println("My favorite number is: ", rand.Intn(10))
+	fmt.Println(quote.Go())
 }
